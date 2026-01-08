324f8595f01a5270f717a5bb30d4aff92b53ad3b
Mateusz Skoczek <mateusz5432160@gmail.com>
2026-01-08 22:25:29 +0100

    repo fix

---
+++ timetabledesigner.website/src/components/pages/HomePage.tsx
@@ -0,0 +1,38 @@
+﻿import logo from "../../resources/logo.svg";
+import Button from "react-bootstrap/Button";
+import React, {Component} from "react";
+
+type Properties = {
+    counter: number;
+};
+
+class HomePage extends Component<{}, Properties> {
+    constructor(props: any) {
+        super(props);
+        this.state = {
+            counter: 0
+        }
+    }
+
+    onButtonClick = () => {
+        this.setState({
+            counter: this.state.counter + 1
+        });
+    }
+
+    render(){
+        return (
+            <div>
+                <header>
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.tsx</code> and save to reload.
+                    </p>
+                    <Button onClick={this.onButtonClick}>counter: {this.state.counter}</Button>
+                </header>
+            </div>
+        );
+    }
+}
+
+export default HomePage;
---
+++ timetabledesigner.website/src/components/pages/RegisterPage.tsx
@@ -0,0 +1,16 @@
+﻿import {Component} from "react";
+
+type Properties = {
+    email: string,
+    password: string,
+}
+
+class RegisterPage extends Component {
+    render(){
+        return (
+            <h1>dfsdaffdsa</h1>
+        );
+    }
+}
+
+export default RegisterPage;
